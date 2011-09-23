804d970f82923674b2f38a978447f911f995892e
Justin Dearing <zippy1981@gmail.com>
2011-09-22 20:50:24 -0400

    Initial commit of a sample exe that launces a second exe as a secondary AppDomain.

---
+++ AppDomainArguments/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace AppDomainArguments
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            AppDomain appDomain = AppDomain.CreateDomain("EchoArguments");
+            var args = new[] {"argument 1", "argument 2", "argument 3"};
+            appDomain.ExecuteAssembly("EchoArguments.exe", null, args);
+            Console.Write("Press Any Key To Continue . . . ");
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ EchoArguments/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace EchoArguments
+{
+    internal static class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i   = 0; i < args.Length; i++)
+            {
+                Console.WriteLine("Arg #: {0} Value: {1}", i, args[i]);
+            }
+        }
+    }
+}
